b98fe25d9bafa628856e899ba7ea96d004b927d5
Franssicca <franssicca@qq.com>
2026-02-26 20:22:14 +0800

    Add WarmupCosineScheduler for learning rate adjustment
    Implement a learning rate scheduler with warmup and cosine annealing.

---
+++ scheduler.py
@@ -0,0 +1,42 @@
+"""
+学习率调度模块
+"""
+
+import math
+import torch.optim as optim
+from torch.optim.lr_scheduler import _LRScheduler
+from config.config import Config
+
+class WarmupCosineScheduler(_LRScheduler):
+    """Cosine annealing with warmup"""
+    def __init__(self, optimizer, warmup_epochs, total_epochs, warmup_lr=1e-6, base_lr=0.01, last_epoch=-1):
+        self.warmup_epochs = warmup_epochs
+        self.total_epochs = total_epochs
+        self.warmup_lr = warmup_lr
+        self.base_lr = base_lr
+        super(WarmupCosineScheduler, self).__init__(optimizer, last_epoch)
+    
+    def get_lr(self):
+        if self.last_epoch < self.warmup_epochs:
+            alpha = self.last_epoch / self.warmup_epochs
+            lr = self.warmup_lr + (self.base_lr - self.warmup_lr) * alpha
+            return [lr for _ in self.base_lrs]
+        else:
+            progress = (self.last_epoch - self.warmup_epochs) / (self.total_epochs - self.warmup_epochs)
+            lr = self.base_lr * 0.5 * (1.0 + math.cos(math.pi * progress))
+            return [lr for _ in self.base_lrs]
+
+def get_scheduler(optimizer):
+    """获取学习率调度器"""
+    if Config.SCHEDULER == 'cosine':
+        scheduler = WarmupCosineScheduler(
+            optimizer,
+            warmup_epochs=Config.WARMUP_EPOCHS,
+            total_epochs=Config.EPOCHS,
+            warmup_lr=Config.WARMUP_LR,
+            base_lr=Config.BASE_LR
+        )
+    else:
+        scheduler = None
+    
+    return scheduler
